bbcd1d3bd8962941c3e0c4fd1ebdb4e04dccf717
Artur Nedyak <advokat1707@gmail.com>
2021-02-09 23:07:19 +0200

    1

---
+++ js/bot.js
@@ -0,0 +1,5 @@
+const guestName = 'Манго';
+const roomNumber = 207;
+const greeting = `Добро пожаловать ${guestName}, вы поселены в номер ${roomNumber}`;
+
+console.log(greeting);
---
+++ js/test.js
@@ -0,0 +1,16 @@
+const first = 300;
+
+console.log(first);
+
+
+const age = 10;
+const totalPrice = 200.74;
+const userName = 'Chelsy';
+const message = 'Добро пожаловать!';
+const isOpen = true;
+const shouldConfirm = false;
+
+console.log(age);
+console.log(totalPrice);
+console.log(userName);
+console.log(isOpen);
